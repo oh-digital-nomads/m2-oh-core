7a7cf8706b59a27ae1efc29897f7be981a8d5953
Franco <franconicolasocchi@gmail.com>
2023-05-10 17:42:26 +0200

    Menu + fix composer php

---
+++ Controller/Adminhtml/Docs/Index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OH\Core\Controller\Adminhtml\Docs;
+
+use Magento\Backend\App\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setUrl($this->getDocsUrl());
+        return $resultRedirect;
+    }
+
+    private function getDocsUrl()
+    {
+        return 'https://docs.ohecommerce.agency/';
+    }
+}
