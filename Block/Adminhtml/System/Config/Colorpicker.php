64938b152b2f66c7c85f528584a8f1ce0c48ef68
Franco <franconicolasocchi@gmail.com>
2023-05-17 12:06:17 +0200

    Color picker block system config

---
+++ Block/Adminhtml/System/Config/Colorpicker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OH\Core\Block\Adminhtml\System\Config;
+
+class Colorpicker extends \Magento\Config\Block\System\Config\Form\Field
+{
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+
+        $html .= '<script type="text/javascript">
+            require(["jquery","jquery/colorpicker/js/colorpicker"], function ($) {
+                $(document).ready(function () {
+                    var thisElement = $("#' . $element->getHtmlId() . '");
+                    thisElement.css("backgroundColor", "' . $value . '");
+                    thisElement.ColorPicker({
+                        color: "' . $value . '",
+                        onChange: function (hsb, hex, rgb) {
+                            thisElement.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+        return $html;
+    }
+}
